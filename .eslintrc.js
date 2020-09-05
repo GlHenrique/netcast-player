7c4a0c0f5ea4cfb6ec3203fe9593e397c4bb43d4
Glênio Carvalho <gleniohenrique-carvalho@hotmail.com>
2020-09-05 14:45:39 -0300

    feat: ✅ add Eslint, Husky, Commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,35 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    "prettier"
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.tsx', '.ts'],
+      },
+    ],
+    'prettier/prettier': 'error',
+    'react/jsx-indent': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+  },
 };
